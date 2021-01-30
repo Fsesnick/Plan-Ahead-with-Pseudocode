8b110405d185ee1b91eb117ad628bb17ecaafb5c
Fsesnick <61622322+Fsesnick@users.noreply.github.com>
2021-01-30 23:02:15 +0100

    Writing the JavaScript Code

---
+++ gpa.js
@@ -0,0 +1,21 @@
+// function calculate_gpa
+    // pass in student_grades
+    // set grade_total to 0
+    // for each grade in student_grades
+        // if grade is not a 1, 2, 3, or 4
+            // print "invalid grade"
+            // iprint grade
+            // print "can't complete calculation"
+            // exit function
+        // else add grade to grade_total
+        // iendif
+    // endfor
+    // set gpa to grade_total / number of grades
+    // return gpa
+// endfunction
+
+// set reggie_grades to 4, 4, 3, 4
+// print the results of calling calculate_gpa with reggie_grades
+
+// set dave_grades to 1, 2, 3, 2
+// print the results of calling calculate_gpa with dave_grades
